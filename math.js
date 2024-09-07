5f735a493ac77723e71514deaf5b539923e5b12c
Habibur Rahman <habibur88454@gmail.com>
2024-09-07 21:57:28 +0600

    math floor ceil random round abs etc. apply

---
+++ math.js
@@ -0,0 +1,14 @@
+
+const maximum = Math.max(12, 34, 56, 43, 324, 23, 435, 2);
+const minimum = Math.min(12, 34, 56, 43, 324, 23, 435, 2);
+// console.log(maximum, minimum)
+
+// console.log(Math.abs(10-41)) // value negative holeo setake positive kore fele
+console.log(Math.round(2.6767))
+console.log(Math.round(2.1767))
+console.log(Math.floor(2.767))
+console.log(Math.ceil(2.767))
+console.log(Math.random())
+console.log(Math.random() * 10)
+const integer = Math.round(Math.random() * 10)
+console.log(integer);
